ab8248f3c6a2fc2576bdf9683809fed35450bc5e
chi2015 <sergeychi2017@gmail.com>
2023-11-13 13:18:08 +0100

    refactor component

--- src/components/DateMonth.js
+++ src/components/DateMonth.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-
-
-export default class DateMonth extends React.Component {
-  getMonthStr() {
-	  switch (parseInt(this.props.month)) {
-			case 0: return this.props.mode == "option" ? "All months" : "";
+const DateMonth = props => {
+	const { month, mode } = props;
+	const getMonthStr = () => {
+	switch (parseInt(month)) {
+			case 0: return mode == "option" ? "All months" : "";
 			case 1: return "January";
 		  case 2: return "February";
 		  case 3: return "March";
@@ -18,15 +17,13 @@ export default class DateMonth extends React.Component {
 		  case 10: return "October";
 		  case 11: return "November";
 		  case 12: return "December";
-	  }
-	  
+	  }	  
 	  return "Month";
-	  
-  }
-  
-  render() {
-    var month_strval = parseInt(this.props.month) < 10 ? "0"+parseInt(this.props.month) : this.props.month;
-    if (this.props.mode == "option") return (<option value={month_strval}>{this.getMonthStr()}</option>);
-    else return (<span>{this.getMonthStr()}</span>);
-  }
+};
+
+	const month_strval = parseInt(month) < 10 ? "0"+parseInt(month) : month;
+	
+	return mode === "option" ? <option value={month_strval}>{getMonthStr()}</option> : <span>{getMonthStr()}</span>
 }
+
+export default DateMonth;
