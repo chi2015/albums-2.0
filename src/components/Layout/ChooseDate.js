72b24449a33fe72e43e03fd9fbdc0b23c94f1859
chi2015 <sergeychi2017@gmail.com>
2023-11-13 13:17:33 +0100

    refactor component

--- src/components/Layout/ChooseDate.js
+++ src/components/Layout/ChooseDate.js
@@ -1,45 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DateMonth from "../DateMonth";
 
-export default class ChooseDate extends React.Component {
-
-	constructor(props) {
-		super(props);
-		this.state = { year : props.year, month : props.month, mode : props.mode };
+const ChooseDate = props => {
+	const { mode } = props;
+	const [year, setYear] = useState(props.year);
+	const [month, setMonth] = useState(props.month);
+	
+	useEffect(() => {
+		setYear(props.year)
+		setMonth(props.month)
+	}, [props.year, props.month]);
+	
+	const handleChangeMonth = e => {
+    	props.changeDate(year, e.target.value);
+    };
+    
+    const handleChangeYear = e => {
+    	props.changeDate(e.target.value, month);
+    };
+    
+    const currentYear = new Date().getFullYear();
+    
+    var year_list = [];
+    if (mode == "list") year_list.push(<option key={0} value={0}>All years</option>);
+    for (let y=currentYear; y>=2000; y--) year_list.push(<option key={y} value={y}>{y}</option>);
+    var month_list = [];
+    for (let m= mode == "list" ? 0 : 1; m<=12; m++) {
+		month_list.push(<DateMonth key={m} month={m} mode="option"/>);
 	}
 	
-	componentWillReceiveProps(nextProps) {
-		this.setState({ year : nextProps.year, month : nextProps.month });
-	}
-
-    handleChangeMonth(e) {
-    	this.props.changeDate(this.props.year, e.target.value);
-    }
-    
-    handleChangeYear(e) {
-    	this.props.changeDate(e.target.value, this.props.month);
-    }
-    
-  render() {
-    const currentYear = new Date().getFullYear();
-    var year_list = [];
-    if (this.state.mode == "list") year_list.push(<option key={0} value={0}>All years</option>);
-    for (var year=currentYear; year>=2000; year--) year_list.push(<option key={year} value={year}>{year}</option>);
-    var month_list = [];
-    for (var month=this.state.mode == "list" ? 0 : 1; month<=12; month++) {
-		var month_str = month < 10 ? "0"+month : month;
-		month_list.push(<DateMonth key={month} month={month} mode="option"/>);
-	}
-    
     return (
       <div className="choose-date-block">
-      	<select value={this.state.month} onChange={this.handleChangeMonth.bind(this)} name="month">
+      	<select value={month} onChange={handleChangeMonth} name="month">
          {month_list}
         </select>
-      	<select value={this.state.year} onChange={this.handleChangeYear.bind(this)} name="year">
+      	<select value={year} onChange={handleChangeYear} name="year">
          {year_list}
         </select>
       </div>
     );
-  }
 }
+
+export default ChooseDate;
